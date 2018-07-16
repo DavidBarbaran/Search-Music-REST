0dd04d3db570c8fd38fcbb3312b900e56b3b467a
JDavidBarbaran <alejandro.david.barbaran@gmail.com>
2018-07-15 21:06:07 -0500

    Diseñar Home

--- app/src/main/java/davidbarbaran/searchmusicrest/SplashActivity.kt
+++ app/src/main/java/davidbarbaran/searchmusicrest/SplashActivity.kt
@@ -1,12 +1,29 @@
 package davidbarbaran.searchmusicrest
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import butterknife.BindView
+import butterknife.ButterKnife
+import com.airbnb.lottie.LottieAnimationView
 
 class SplashActivity : AppCompatActivity() {
+
+    @BindView(R.id.splash_animation)
+    lateinit var splashAnimation: LottieAnimationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        ButterKnife.bind(this)
+        splashAnimation.addAnimatorListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                super.onAnimationEnd(animation)
+                finish()
+                startActivity(Intent(this@SplashActivity, HomeActivity::class.java))
+            }
+        })
     }
 }
